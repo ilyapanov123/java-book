290ff64ec7aa8dc28770ead47e3e40e4f33fe79e
ilyapanov123 <stoppoler@gmail.com>
2026-04-12 17:11:52 +0300

    start

---
+++ src/test/java/org/example/springg/SprinGgApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinGgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
